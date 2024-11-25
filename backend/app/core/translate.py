0b94bfeeddc2dbb1ee059a77edfe6113a63c80a7
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-11-25 13:36:49 -0400

    ✨ [ADD] translation model

--- backend/app/core/translate.py
+++ backend/app/core/translate.py
@@ -1,12 +1,67 @@
+# Libraries
 from bs4 import BeautifulSoup
+from unsloth.chat_templates import get_chat_template
+from unsloth import FastLanguageModel
+import torch
 
+# Models
 from app.models.translate_request import TargetLanguage
 
+# Instances
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = torch.float16 # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "AleNunezArroyo/Llama-3.2-3B-es_qu-S400", # or choose "unsloth/Llama-3.2-1B-Instruct"
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+)
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "llama-3.1",
+)
+
 def translate_text(text: str, target_language: TargetLanguage) -> str:
-    if target_language == "esp":
-        return f"Traducción simulada al español: {text}"
-    elif target_language == "que":
-        return f"Traducción simulada al quechua: {text}"
+    if target_language == "que":
+        messages = [
+            {"role": "user", "content": f"<|lang:es|>“{text}”<|lang:es|>"},
+        ]
+        FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+        inputs = tokenizer.apply_chat_template(
+            messages,
+            tokenize = True,
+            add_generation_prompt = True, # Must add for generation
+            return_tensors = "pt",
+        ).to("cuda")
+
+        outputs = model.generate(
+            input_ids=inputs,
+            max_new_tokens=128,  # Aumentar si la respuesta es muy corta
+            use_cache=True,
+            temperature=0.7,  # Menos aleatorio, más preciso
+            min_p=0.7,  # Mayor probabilidad mínima para generar respuestas más consistentes
+            top_p=0.9,  # Reduce la probabilidad de palabras menos probables
+            top_k=50  # Considera solo los 50 tokens más probables
+        )
+
+        response = tokenizer.batch_decode(outputs)
+
+        print(response[0])
+
+        translation = response[0].split(
+            ":qu|>")[1].split(
+            "<|")[0].strip().replace("“", "").replace("”", "")
+
+        # Check if match
+        if len(translation) == 0:
+            raise ValueError("Traducción no encontrada")
+        return translation
+
+    elif target_language == "esp": # In development...
+        return text
+        # return f"Traducción simulada al quechua: {text}"
     else:
         raise ValueError("Unsupported language")
 
